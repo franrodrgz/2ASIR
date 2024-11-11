49a9429604c8ff276d2f526baace24a08a8879fa
Fran <franaganjunior@gmail.com>
2024-11-11 08:38:45 +0100

    updated

---
+++ HLC/tupli.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def main(args):
+	from operator import itemgetter
+	from decimal import Decimal
+	
+	for i in range(3):
+		
+	return 0
+
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
