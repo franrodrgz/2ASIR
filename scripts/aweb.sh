dbb3dfbd2fa318c73dddf134331ca533c9865ce2
Fran <franaganjunior@gmail.com>
2025-02-18 09:56:59 +0100

    aaaaawww

--- scripts/aweb.sh
+++ scripts/aweb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if mount | grep /home/usuario/shared/vb1 > /dev/null; then
-  ssh user@vb1 -p 22 'sudo chmod -R 755 /var/www/html'
-  sudo umount /home/usuario/shared/vb1
+if mount | grep /home/usuario/webAsir > /dev/null; then
+  ssh user@1 -p 22 'sudo chmod -R 755 /var/www/html'
+  sudo umount /home/usuario/webAsir
   echo "Desconexión realizada correctamente"
 else
-  ssh user@vb1 -p 22 'sudo chmod -R 777 /var/www/html'
-  sudo sshfs -p 22 -o IdentityFile=/home/usuario/.ssh/id_rsa,allow_other,default_permissions user@vb1:/var/www/html /home/usuario/shared/vb1/
+  ssh user@1 -p 22 'sudo chmod -R 777 /var/www/html'
+  sudo sshfs -p 22 -o IdentityFile=/home/usuario/.ssh/id_rsa,allow_other,default_permissions user@1:/var/www/html /home/usuario/webAsir/
   echo "Conexión realizada con éxito"
 fi
